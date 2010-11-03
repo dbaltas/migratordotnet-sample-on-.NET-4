0f640732a3cf68f8036410e45c899bda473ad5ed
Dimitris @ zeus <dimbaltas@yahoo.gr>
2010-11-04 00:28:54 +0200

    Initial Repository

---
+++ 002_Planets.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using Migrator.Framework;
+
+namespace migro
+{
+	[Migration(2)]
+	public class _001_Planets : Migration
+	{
+		public override void Up()
+		{
+			Database.AddTable("Planets",
+				new Column("Id", DbType.Int32, ColumnProperty.PrimaryKeyWithIdentity),
+				new Column("Name", DbType.String, 100, ColumnProperty.NotNull),
+				new Column("Diameter", DbType.Double),
+				new Column("Mass", DbType.Double),
+				new Column("SupportsLife", DbType.Boolean, false)
+			);
+		}
+
+		public override void Down()
+		{
+			Database.RemoveTable("Planets");
+		}
+	}
+}
---
+++ 003_LogEntry.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Migrator.Framework;
+using System.Data;
+
+namespace migro
+{
+	[Migration(3)]
+	public class LogEntry : Migration
+	{
+		public override void Up()
+		{
+			Database.ExecuteNonQuery(
+			@"
+CREATE TABLE [dbo].[ErrorLog](
+	[ErrorLogID] [numeric](18, 0) IDENTITY(1,1) NOT NULL,
+	[ErrorLevelCode] [nvarchar](50) NULL,
+	[ErrorSource] [nvarchar](100) NULL,
+	[ErrorDescription] [nvarchar](1000) NULL,
+	[CreatedBy] [numeric](18, 0) NULL,
+	[CreatedDate] [smalldatetime] NULL CONSTRAINT [DF_ErrorLog_CreatedDate]  DEFAULT (getdate()),
+ CONSTRAINT [PK_ErrorLog] PRIMARY KEY CLUSTERED 
+(
+	[ErrorLogID] DESC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+			");
+		}
+
+		public override void Down()
+		{
+			Database.ExecuteNonQuery(
+@"drop TABLE [dbo].[ErrorLog]"
+				);
+		}
+	}
+}
